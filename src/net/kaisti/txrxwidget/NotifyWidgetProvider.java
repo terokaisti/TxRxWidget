aa2bc408981d40e3050f465f10b063467df51b4f
Tero Kaisti <tero@kaisti.net>
2011-07-27 23:20:03 +0300

    Fixed #2

--- src/net/kaisti/txrxwidget/NotifyWidgetProvider.java
+++ src/net/kaisti/txrxwidget/NotifyWidgetProvider.java
@@ -20,11 +20,16 @@ import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class NotifyWidgetProvider extends AppWidgetProvider {
+	/**
+	 * Stop the reporting service when the widget is mr
+	 */
 	@Override
 	public void onDeleted(Context context, int[] appWidgetIds) {
+		Intent serviceIntent = new Intent(context, NotifyWidgetService.class);
+		context.stopService(serviceIntent);
 		super.onDeleted(context, appWidgetIds);
 	}
-
+	
 	@Override
 	public void onDisabled(Context context) {
 		super.onDisabled(context);
